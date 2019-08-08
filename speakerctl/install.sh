5597eeee569a286804c73049b936bf37befbb5d8
Ivan Dzikovsky <idzikovsky@protonmail.com>
2019-08-08 22:03:00 +0300

    Use one SystemD service for turn on/off

--- speakerctl/install.sh
+++ speakerctl/install.sh
@@ -3,7 +3,6 @@
 sudo cp ./speakerctl /usr/local/bin
 sudo chmod +x /usr/local/bin/speakerctl
 [ ! -e "/var/lib/speakerctl.token" ] && sudo /usr/local/bin/speakerctl login
-sudo cp ./speakerctl-start.service ./speakerctl-shutdown.service /etc/systemd/system/
+sudo cp ./speakerctl.service /etc/systemd/system/
 sudo systemctl daemon-reload
-sudo systemctl enable speakerctl-start
-sudo systemctl enable speakerctl-shutdown
+sudo systemctl enable speakerctl
